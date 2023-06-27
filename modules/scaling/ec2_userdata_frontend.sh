30a5cffe7f1984ad5c8931d1528fe40f0570af65
dp997 <51825500+dp997@users.noreply.github.com>
2023-06-27 22:54:21 +0200

    change frontend userdata to serve react app better

--- modules/scaling/ec2_userdata_frontend.sh
+++ modules/scaling/ec2_userdata_frontend.sh
@@ -2,6 +2,8 @@
 
 # Install awsCLI
 sudo yum -y install awscli
+# Install GIT
+sudo yum -y install git
 # Install gettext
 sudo yum -y install gettext
 # Install jq
@@ -10,7 +12,17 @@ sudo yum -y install jq
 sudo yum -y update; sudo yum clean all
 sudo yum -y install http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm; sudo yum -y makecache
 sudo yum -y install nginx-1.14.0
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash - 
+sudo yum -y install nodejs
 
+mkdir /var/reactapp
+git clone https://github.com/dp997/devops-lab-3-reactapp.git
+cd devops-lab-3-reactapp
+npm install create-react-app
+npm run build
+
+cp -r /devops-lab-3-reactapp/build/* /var/reactapp
 # Remove default files we don't need
 sudo rm -f /etc/nginx/conf.d/default.conf
 
@@ -28,15 +40,22 @@ http {
     upstream myapp {
         server $(aws elbv2 describe-load-balancers --names Backend-LB --region us-east-1 | jq -r .LoadBalancers[0].DNSName);
     }
-
+    include /etc/nginx/mime.types;
     server {
         listen 80 default_server;
         server_name "";
+        
         location / {
-            proxy_pass http://myapp;
+            root /var/reactapp/;
+            index index.html index.htm;
+        }
+        location /app {
+            proxy_pass http://myapp/;
             proxy_set_header Host \$host;
             proxy_http_version 1.1;
             proxy_read_timeout 120s;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-Proto \$scheme;
         }
     }
 }
